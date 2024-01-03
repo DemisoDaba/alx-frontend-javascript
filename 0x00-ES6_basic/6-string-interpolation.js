af2d2b9f8124e6720f6e91bac16b7d6bfabf1ff4
DemisoDaba <demo.nkmt1@gmail.com>
2024-01-03 00:00:52 -0800

    Take advantage of template literals

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -10,3 +10,4 @@ export default function getSanFranciscoDescription() {
 , with a per capita personal income of ${budget.income}. As of 2015, San Francisco \
 proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
+
