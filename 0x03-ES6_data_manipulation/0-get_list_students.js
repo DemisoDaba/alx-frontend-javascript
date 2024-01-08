fe71ba6939866eecb626f3d0f14d765b3f39b45f
DemisoDaba <demo.nkmt1@gmail.com>
2024-01-08 09:37:31 -0800

    a function named getListStudents that returns an array of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,19 @@
-function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
-  ];
+export default function getListStudents() {
+  const arrayOfObjects = [];
+  arrayOfObjects.push({
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  },
+  {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  });
+  return arrayOfObjects;
 }
-
-export default getListStudents;
