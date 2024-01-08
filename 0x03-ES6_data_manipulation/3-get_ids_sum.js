c8beca4fa008b8e72bb788e955e3f54ab59abac5
DemisoDaba <demo.nkmt1@gmail.com>
2024-01-08 09:14:29 -0800

    a function getStudentIdsSum that returns the sum of all the student ids

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+export default function getStudentIdsSum(studentObjects) {
+  if (Array.isArray(studentObjects) === false) {
+    return 0;
+  }
+
+  return studentObjects.reduce((sumIds, student) => sumIds + student.id, 0);
+}
