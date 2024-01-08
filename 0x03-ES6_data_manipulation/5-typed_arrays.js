abd6bde6fdb8e07b4c31fb13cb0575424b181ee1
DemisoDaba <demo.nkmt1@gmail.com>
2024-01-08 09:40:49 -0800

    task 5 correction

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,12 @@
-export default function createInt8TypedArray(bufferLength, position, value) {
-  const buffer = new ArrayBuffer(bufferLength);
-  const int8View = new Int8Array(buffer);
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
 
-  if (position >= int8View.length || position < 0) {
-    throw new Error('Position outside range');
-  }
-  int8View[position] = value;
+  const int8d = new Int8Array(buffer);
+
+  if (position < 0 || position >= length) throw new Error('Position outside range');
+  // throw new RangeError();
+
+  int8d[position] = value;
+
   return new DataView(buffer);
 }
-
